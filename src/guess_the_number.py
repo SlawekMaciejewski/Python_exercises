1a45db8c60b6586e777b391db3dabcfc50d1ab6d
SlawekMaciejewski <slawek74.m@interia.pl>
2020-01-19 09:44:57 +0100

    fixed problem with var. count

--- src/guess_the_number.py
+++ src/guess_the_number.py
@@ -1,12 +1,12 @@
 from random import randint
 
 
-def draw_a_number():
+def draw_a_number() -> int:
     random_number = randint(1, 100)
     return random_number
 
 
-def is_a_number(data):
+def is_a_number(data: str) -> bool:
     try:
         if data.isdigit():
             return True
@@ -22,15 +22,17 @@ if __name__ == "__main__":
     # print(random_number)
     count = 0
     while True:
-        count += 1
-        user_number = str(input('Guess number 1 to 100, enter a integer: ').strip())
+        user_number = input('Guess number 1 to 100, enter a integer: ').strip()
+        # print(user_number)
         if not is_a_number(user_number):
             continue
+        count += 1
         if int(user_number) == random_number:
             print(f'You guessed!!! This number is: {random_number}. You guessed it {count} times')
             answer = input('Do you try again? \nPress "Y/y"(yes) or any key to (no) : ').strip()
             if answer.upper() == 'Y':
                 random_number = draw_a_number()
+                count = 0
                 # print(random_number)
                 continue
             else:
