06cb360d41ca0b5cbbac0fe23b6627998cd8a55f
SlawekMaciejewski <slawek74.m@interia.pl>
2020-02-01 20:08:38 +0100

    Added checking functions isinstance() and issubclass() to oop_car.py

--- oop/oop_car.py
+++ oop/oop_car.py
@@ -25,6 +25,18 @@ class Car:
         self.in_motion = False
 
 
+class Truck(Car):
+    acceleration = 5
+
+    def __init__(self, reg_number, number_of_wheels):
+        super().__init__(reg_number)
+        self.number_of_wheels = number_of_wheels
+
+
 if __name__ == '__main__':
     toyota_avensis = Car('EZD 99')
     toyota_avensis.print_reg_number()
+    print(isinstance(toyota_avensis, Car))
+    truck1 = Truck('EZD1234', 10)
+    print(truck1.number_of_wheels)
+    print(issubclass(Truck, Car))
